e831076d78b8f878f5750953b7c4b55f65fda596
samismooth <samantha.n@outlook.com.ar>
2023-04-27 19:19:12 -0300

    first commit

---
+++ Java.js
@@ -0,0 +1,16 @@
+
+function Datos (nombre, edad, domicilio){
+    nombre = nombre;
+    edad = edad;
+    domicilio = domicilio;
+}
+
+
+Datos.nombre = prompt("Ingrese su nombre:")
+
+Datos.edad = prompt("Ingrese su edad:")
+
+Datos.domicilio = prompt("Ingrese su domicilio:")
+
+
+alert ("Nombre: " + Datos.nombre + "\nEdad: " + Datos.edad + "\nDomicilio: " + Datos.domicilio)
